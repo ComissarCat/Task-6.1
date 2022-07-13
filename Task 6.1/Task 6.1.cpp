94f18234f67215bf72226c2b53972f3f909e1ee0
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2022-07-13 09:26:30 +0300

    Добавлена функция возведения в степень

---
+++ Task 6.1/Task 6.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int pow(int basis, int degree);
+int sum_from_range(int start, int end);
+int perfect_from_range(int start, int end);
+void draw_cart(char suit, char value);
+void lucky_number(int number);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    cout << pow(2, 10);
+
+    return 0;
+}
+
+int pow(int basis, int degree)
+{
+    int result = basis;
+    for (int i = 1; i < degree; i++)
+    {
+        result *= basis;
+    }
+    return result;
+}
