5998d5f9c7d43f167acf86c9d851c021213f8a33
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2022-07-13 10:16:03 +0300

    Добавлена функция поиска совершенных чисел в диапазоне

--- Task 6.1/Task 6.1.cpp
+++ Task 6.1/Task 6.1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int pow(int basis, int degree);
 int sum_from_range(int start, int end);
-int perfect_from_range(int start, int end);
+void perfect_from_range(int start, int end);
 void draw_cart(char suit, char value);
 void lucky_number(int number);
 
@@ -11,7 +11,7 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    cout << sum_from_range(1, 5);
+    perfect_from_range(1, 500);
 
     return 0;
 }
@@ -34,4 +34,17 @@ int sum_from_range(int start, int end)
         result += i;
     }
     return result;
+}
+
+void perfect_from_range(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {        
+        int sum_of_dividers = 0;        
+        for (int j = 1; j <= i; j++)
+        {
+            if (not(i % j)) sum_of_dividers += j;
+        }
+        if ((sum_of_dividers - i) == i) cout << i << "\t";
+    }
 }
